5eae90796e534b599f93e4421c9ccff8cd1f4dc5
Romari01 <romarioquispe214@gmail.com>
2026-05-14 05:15:38 -0500

    fix cors produccion

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,11 @@ const errorHandler = require('./middleware/errorHandler')
 const app = express()
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://vita-care-amber.vercel.app',
+    process.env.CLIENT_URL
+  ],
   credentials: true
 }))
 app.use(express.json())
